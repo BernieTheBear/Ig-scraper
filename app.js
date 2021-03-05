f462cf82a510622c2a7bdef372ea1588305399c0
BernieTheBear <balmanzar883@gmail.com>
2021-03-04 22:58:30 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,106 +1,15 @@
-/* RESOURCES 
-  https://www.selenium.dev/selenium/docs/api/dotnet/html/Methods_T_OpenQA_Selenium_Chrome_ChromeDriver.htm
 
+const scraper = require('./insta-scrape-test.js')
+const express = require('express')
 
-  //finding elements
-  https://www.selenium.dev/selenium/docs/api/dotnet/html/Methods_T_OpenQA_Selenium_Chrome_ChromeDriver.htm
-  https://www.selenium.dev/documentation/en/getting_started_with_webdriver/locating_elements/
-
-
-  https://www.codota.com/code/javascript/functions/selenium-webdriver/By/className
-
-
-Immediatley invoked function such as this below, need a semi colon on the thing you are requiring
-(async function example(){})();
-"For me, when I do Immediately invoked function, I need to put ; at the end of require()."
-
-https://www.youtube.com/watch?v=khwV5IWng-I&ab_channel=GaurAssociates
+const app = express()
 
 
 
-https://sqa.stackexchange.com/questions/26422/how-to-get-href-attribute-value-using-selenium-java
-https://stackoverflow.com/questions/20888592/gettext-method-of-selenium-chrome-driver-sometimes-returns-an-empty-string
 
-https://www.guru99.com/locate-by-link-text-partial-link-text.html
-
-SELECT - accessing values in dropdown
-**** https://stackoverflow.com/questions/26041791/how-to-select-a-dropdown-value-in-selenium-webdriver-using-node-js
-https://www.guru99.com/select-option-dropdown-selenium-webdriver.html
-https://www.browserstack.com/guide/select-class-in-selenium
-
-
-*/
+const celebChoice = 'cristiano'
+scraper.main_scrape_func('bhurnalcodes','s15koukie39',celebChoice)
 
 
 
-// /* Simple working ex.*/
-// var webdriver = require('selenium-webdriver');
-// require('chromedriver')
-
-// const test = async()=>{
-//   var driver = new webdriver.Builder().forBrowser('chrome').build();
-//   await driver.get('http://google.com')
-//   await driver.sleep(100)
-//   driver.quit();
-// }
-// test()
-//-------------------------
-
-// const webdriver = require('selenium-webdriver');
-// const {By} = require('selenium-webdriver');
-// //webdriver.Options.setExperimentalOption('detach',true)
-
-// (async function example(){
-//     let driver = await new webdriver.Builder().forBrowser('chrome').build();
-//     try{
-//       await driver.get('https://www.megamillions.com/Winning-Numbers/Previous-Drawings.aspx')
-//       let element =(await driver.findElement(By.className('prevDrawingList'))).getText();
-//       console.log('This is a test' + element)
-//        //driver.sleep(100)
-      
-//     } catch(err){
-//         console.log(err)
-//     } finally {
-//         //await driver.quit()
-//     }
-// })();
-
-
-//=========================================================================
-//const webDriver = require('selenium-webdriver');
-const {By,Builder,Key,util,withTagName,cssSelector,Select, WebDriver} = require('selenium-webdriver');
-const { elementIsDisabled } = require('selenium-webdriver/lib/until');
-
-(async() =>{
-    let driver = await new Builder().forBrowser('chrome').build();
-   await driver.get('https://google.com')
-   await driver.findElement(By.name('q')).sendKeys('megamillions',Key.RETURN);
-  
-   
-   //google search mega millions, click megamillions
-   let div = await driver.findElement(By.className('yuRUbf'))
-   let t = await (await div.findElement(By.css('a'))).getAttribute('href')
-   await driver.get(t)
-
-
-   //from megamillions home page, targeted 'past winning number' to retrieve href, and navigate to it
-   let homeWinnerBanner = await driver.findElement(By.className('homeWinnerLeft'))
-   let pastWinningNumButton = await (await homeWinnerBanner.findElement(By.className('button fi btnPastWin'))).getAttribute('href')
-   await driver.get(pastWinningNumButton)
-
-  //from past winning page, select both date boxes with clicking
-  let startDate = await (await driver.findElement(By.id('drawsStartDate'))).click()
-  let dateBoxdropDown = await driver.findElement(By.css('#drawsStartDate_root > div > div > div > div > div.picker__header > select > option:nth-child(1)'))
-  dateBoxdropDown.click();
-  console.log(dateBoxdropDown.getAttribute('innerText'))
-  
-   //dateBoxdropDown.findElement()> .picker__select--year > option:nth-child(1)'))
-  //dateBox.click()
-  
-
-})().then((t)=>{
-    console.log(t);
-}).catch((err)=>{
-  console.log(err)
-});
 
--- insta-scrape-test.js
+++ insta-scrape-test.js
@@ -22,100 +22,165 @@ https://www.selenium.dev/selenium/docs/api/javascript/module/selenium-webdriver/
 CSS 2.1 allows authors to specify rules that match elements which have certain attributes defined in the source document.
 https://www.w3.org/TR/CSS2/selector.html#adjacent-selectors
 
+
+SELECTING CHILDREN OF NODE
+https://stackoverflow.com/questions/5119655/xpath-to-get-all-child-nodes-elements-comments-and-text-without-parent/5119937
+
+Await examples
+https://medium.com/@giltayar/javascript-asynchrony-and-async-await-in-selenium-webdriver-tests-a89924421f65
 */
 
 
 
 
-const {By,Builder,Key,util,withTagName,cssSelector,Select, WebDriver,until} = require('selenium-webdriver');
+const {By,Builder,Key,util,withTagName,cssSelector,Select, WebDriver,until, promise,Promise,Map,map} = require('selenium-webdriver');
 const { elementIsDisabled } = require('selenium-webdriver/lib/until');
+const fs = require('fs')
 
-(async() =>{
-    let driver = await new Builder().forBrowser('chrome').build();
-  
-    await driver.get('https://instagram.com');
-    await driver.sleep(2000)
 
-    const username = await driver.wait(until.elementLocated(By.name('username')),3000);
-    username.sendKeys('bhurnalcodes') 
+async function main_scrape_func(un,pw,celebChoice){
+      let driver = await new Builder().forBrowser('chrome').build();
     
-    const password = await driver.wait(until.elementLocated(By.name('password')),5000);
-    password.sendKeys('s15koukie39') 
+      await driver.get('https://instagram.com');
+      await driver.sleep(2000)
 
-    await driver.sleep(2000)
-    const loginButton = await driver.wait(until.elementLocated(By.className('sqdOP  L3NKy   y3zKF')),3000);
-    loginButton.click();
-
-    // //to clear save login info by clicking "not now"
-    // await driver.sleep(2000)
-    // const saveLoginInfoButton =  await driver.wait(until.elementLocated(By.className('cmbtv')),3000);
-    // await saveLoginInfoButton.click();
-
-    //to clear turn on post notif button
-    await driver.sleep(2000)
-    const postNotifButton =  await driver.wait(until.elementLocated(By.className('aOOlW   HoLwm ')),3000);
-    await postNotifButton.click();
-    
-
-    //once logged in, go directly to celeb profile via url. 
-    /* The other option here is the manually enter the celebs name in the search bar and click the first result. I tried this and it was tricky 
-        This was the code. It would enter the celeb name and then hit enter, then enter again, this would go to the actual celebs page
-
-            // const searchButton =  await driver.wait(until.elementLocated(By.className('XTCLo x3qfX')));
-            // await searchButton.sendKeys('Kim Kardashian',Key.RETURN)
-            // driver.sleep(1000)
-            // await searchButton.sendKeys(Key.RETURN)
-            // //await searchButton.sendKeys(Key.RETURN)
-            // //await searchButton.sendKeys(Key.RETURN)
-    */
-    await driver.sleep(3000)
-    await driver.get('https://www.instagram.com/cristiano/');
-    
-
-    //clicking first post
-    await driver.sleep(2000)
-    const z = await driver.findElement(By.css('#react-root > section > main > div > div._2z6nI > article > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1)'))
-    z.click()
-    
-
-    //clicking load more
-    await driver.sleep(2300)
-    const loadMore = await driver.findElement(By.css('body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > div.EtaWk > ul > li > div'))
-    loadMore.click();
-
-    await driver.sleep(2300)
-    loadMore.click();
-  
-    await driver.sleep(2300)
-    loadMore.click();
-
-    await driver.sleep(2300)
-    loadMore.click();
-
-    //targeting commments
-    /* Goal:  target by 'Mr508', getting full html and parse the comments out of the html later*/
-    let testArray;
-    let arrayComments;
-    // const commentsList = await (await driver.findElement(By.xpath('/html/body/div[4]/div[2]/div/article/div[3]/div[1]/ul'))).getAttribute('innerText')
-    // console.log(commentsList)
-    
-    //targeting list of comments UL class XQXOT pxf-y
-    const commentsList = await (await driver.findElement(By.xpath('/html/body/div[4]/div[2]/div/article/div[3]/div[1]/ul')));
-
-    //get all children 
-    testArray = await commentsList.findElements(By.xpath('./child::*'))
-    testArray.forEach(async(element)=>{
-      let temp2 = await (await element.findElement(By.css('.C4VMK > span'))).getAttribute('innerText')
-      console.log(temp2)
+      const username = await driver.wait(until.elementLocated(By.name('username')),1000);
+      username.sendKeys('bhurnalcodes') 
       
-      //arrayComments.push(temp2)
-    })
-    console.log(arrayComments)
-    
-  
-})().then((t)=>{
-    console.log(t);
-}).catch((err)=>{
-  console.log(err)
-});
+      const password = await driver.wait(until.elementLocated(By.name('password')),1000);
+      password.sendKeys('s15koukie39') 
 
+      await driver.sleep(2000)
+      const loginButton = await driver.wait(until.elementLocated(By.className('sqdOP  L3NKy   y3zKF')),2000);
+      loginButton.click();
+
+      // //to clear save login info by clicking "not now"
+        await driver.sleep(2000)
+        const saveLoginInfoButton =  await driver.wait(until.elementLocated(By.className('cmbtv')),3000);
+        await saveLoginInfoButton.click();
+
+      //to clear turn on post notif button
+      await driver.sleep(1000)
+      const postNotifButton =  await driver.wait(until.elementLocated(By.className('aOOlW   HoLwm ')));
+      await postNotifButton.click();
+      
+
+      //once logged in, go directly to celeb profile via url. 
+      /* The other option here is the manually enter the celebs name in the search bar and click the first result. I tried this and it was tricky 
+          This was the code. It would enter the celeb name and then hit enter, then enter again, this would go to the actual celebs page
+
+              // const searchButton =  await driver.wait(until.elementLocated(By.className('XTCLo x3qfX')));
+              // await searchButton.sendKeys('Kim Kardashian',Key.RETURN)
+              // driver.sleep(1000)
+              // await searchButton.sendKeys(Key.RETURN)
+              // //await searchButton.sendKeys(Key.RETURN)
+              // //await searchButton.sendKeys(Key.RETURN)
+      */
+      await driver.sleep(3000)
+      await driver.get(`https://www.instagram.com/${celebChoice}/`);
+      
+
+      //clicking first post
+      await driver.sleep(2000)
+      const z = await driver.findElement(By.css('#react-root > section > main > div > div._2z6nI > article > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1)'))
+      z.click()
+      
+     
+
+//=================================================================================================
+     
+  /*TARGETING COMMENTS SECTION
+      Goal:  Target UL of all comments - class: XQXOT pxf-y,
+             Then in the UL, get all Mr508 elements, these are comments
+
+              Then */
+  
+
+          // const commentsList = await (await driver.findElement(By.xpath('/html/body/div[4]/div[2]/div/article/div[3]/div[1]/ul'))).getAttribute('innerText')
+          // console.log(commentsList)
+      
+     async function scrapeCommentsFromPost () {   
+
+          //clicking load more
+        await driver.sleep(2300)
+        const loadMore = await driver.findElement(By.css('body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > div.EtaWk > ul > li > div'))
+        loadMore.click();
+
+        await driver.sleep(2300)
+        loadMore.click();
+        await driver.sleep(2300)
+        loadMore.click();
+        await driver.sleep(2300)
+        loadMore.click();
+
+        //**targeting list of comments UL class: 'XQXOT pxf-y' , this returns a web element promise
+        let commentListRootPromise = await driver.findElement(By.css('body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > div.EtaWk > ul'));
+      
+        //** Returns an array of promises -specifically getting all elements that ar just Mr508, these are the comments */
+        let commentListChildren = await commentListRootPromise.findElements(By.className('Mr508'))
+
+        let arrayComments = new Array();
+        /**Iterate each Mr508, find span, and extract inner text, this is the pure comment text, 
+          then push to arrayComments */
+
+        for(i=0;i<commentListChildren.length;i++){
+          //**get span section of 508 to then get text from span using 'innerText' NEED TO DO THIS FOR EACH 508 element
+            let spanText508 = await commentListChildren[i].findElement(By.css('.C4VMK > span'))
+          //**then get innerText
+            let innerText = await spanText508.getAttribute('innerText').then((text)=>{
+                //console.log('this is just the text: '+ text) -FOR TESTING
+                //innerText = text; FOR TESTING
+                return text
+            })
+            //console.log('this is innnerText: ' + innerText)- FOR TESTING
+            arrayComments.push(innerText)
+        }
+       
+        //console.log('m length is '+ arrayComments.length)
+        //console.log('this is arrayComments contents: ' + arrayComments) //checking of of array contents
+        return arrayComments;
+    }
+//--------------------------------------------------------------  
+//WRITE COMMENTS FROM ARRAYCOMMENTS TO FILE
+    function writeToFile(comments){
+      comments.forEach((element)=>{
+        elementSpace = element + ', '
+          fs.appendFile('comments.txt',elementSpace,(err)=>{
+            if(err){
+              console.log('error')
+            }
+          })
+        })
+     }
+//------------------------------------------------------------------
+//==================================================================
+
+     /** function that click next arrow to get next post and begin scrape again */
+    async function nextPost (){
+      let nextPostArrow = await driver.findElement(By.css('body > div._2dDPU.CkGkG > div.EfHg9 > div > div > a'))
+      await driver.sleep(1500)
+      nextPostArrow.click();
+      console.log(':::::GOING TO NEXT POST::::::')
+    }
+//=================================================================
+/** CORE APP FUNCTIONALITY */
+
+
+  let scrapedComments = await scrapeCommentsFromPost()
+  console.log(scrapedComments + ' length: ' + scrapedComments.length)
+  writeToFile(scrapedComments)
+  nextPost();
+  scrapedComments = await scrapeCommentsFromPost()
+  writeToFile(scrapedComments)
+  console.log(scrapedComments + ' length: ' + scrapedComments.length)
+//-------------------------------
+}
+//main_scrape_func()
+//======================================================
+
+
+
+module.exports = {
+    main_scrape_func: main_scrape_func,
+}
+  
